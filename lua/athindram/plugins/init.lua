60c25284f28e813c3982223f25d204fc73d2a4bf
Athindra Mitra <athindra.nm4@gmail.com>
2025-10-14 19:43:43 -0400

    added telescope fuzzy finder (very cool)

--- lua/athindram/plugins/init.lua
+++ lua/athindram/plugins/init.lua
@@ -1,4 +1,9 @@
 return {
-  "nvim-lua/plenary.nvim", -- lua funcitons that many plugins use
-  "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  { 
+    "nvim-lua/plenary.nvim", -- lua funcitons that many plugins use
+    lazy = false
+  },
+  {
+    "christoomey/vim-tmux-navigator", -- tmux & split window navigation
+  },
 }
