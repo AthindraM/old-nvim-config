49fbb451f57a2ba12b0e80c2431185ec7c9f1a95
Athindra Mitra <athindra.nm4@gmail.com>
2025-10-14 22:43:54 -0400

    added lualine

--- lua/athindram/lazy.lua
+++ lua/athindram/lazy.lua
@@ -11,4 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("athindram.plugins")
+require("lazy").setup("athindram.plugins", {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+})
